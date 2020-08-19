88bc70e1ec5050c8018154fd78986d1082332330
Nam Nguyen <namnguyen.sci@gmail.com>
2020-08-20 00:06:27 +0700

    Testing root page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
+import myLogo from './assets/images/topic-target.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={myLogo} className="App-logo" alt="myLogo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Be like a postage stamp stick to one thing until you get there
         </p>
-        <p>Tim hieu them: <a href="https://viblo.asia/p/deploy-create-react-app-project-to-github-pages-m68Z0R465kG">deploy-create-react-app-project-to-github-pages</a></p>
+        <p>Learn more: <a href="https://viblo.asia/p/deploy-create-react-app-project-to-github-pages-m68Z0R465kG">deploy-create-react-app-project-to-github-pages</a></p>
         <a
           className="App-link"
           href="https://reactjs.org"
